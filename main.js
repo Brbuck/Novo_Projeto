c9040331867c93cc55ae7e24d541589615eacec7
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-04-06 14:08:19 -0300

    Update CSS e JS

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ let toggle = document.querySelector('.toggle')
 let exit = document.querySelector('.exit')
 
 toggle.addEventListener('click', () =>{
-    menu.classList.toggle('showmenu')
+    menu.classList.add('showmenu')
+    exit.classList.add('showexit')
+    toggle.classList.add('none')
+})
+
+exit.addEventListener('click', () =>{
+    menu.classList.remove('showmenu')
+    exit.classList.remove('showexit')
+    toggle.classList.remove('none')
 })
 
 
